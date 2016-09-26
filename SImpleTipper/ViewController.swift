934dd4289e2dad132c7d14b9326ab2fecd342a4d
Alexey Rastaturin <alexey@alexeys-mbp>
2016-09-26 08:27:23 -0700

    Calculator, some formatting.

--- SImpleTipper/ViewController.swift
+++ SImpleTipper/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var precentageSelector: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func dataChanged(_ sender: AnyObject) {
+        print(precentageSelector.selectedSegmentIndex)
+        
+        let tipPercentage = [0.18, 0.20, 0.25];
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentage[precentageSelector.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
